b4d96a775579f9c6b18c7b2a15a0a53f13427fa5
Benjamin Riviere <benjamin.riviere@linkfluence.com>
2020-04-11 21:13:56 +0200

    Add Angular Material to project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { CoreModule } from '@app/core.module';
+import { SharedModule } from '@shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,8 +16,13 @@ import { AppComponent } from './app.component';
     // Angular
     BrowserModule,
 
+    // Core & Shared
+    CoreModule,
+    SharedModule,
+
     // App
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MaterialModule } from './material.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+
+    MaterialModule
+  ]
+})
+export class SharedModule { }
