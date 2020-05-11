10c092767d19b4e8c7b338ba547875fcfb1d1c7b
Benjamin Riviere <benjamin.riviere@linkfluence.com>
2020-05-11 09:12:08 +0200

    Change N/A value for duration pipe.

--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DurationPipe implements PipeTransform {
   transform(value: number | null): string {
     if (value === null) {
-      return 'N/A';
+      return '--:--:--';
     }
 
     const date = new Date(null);
